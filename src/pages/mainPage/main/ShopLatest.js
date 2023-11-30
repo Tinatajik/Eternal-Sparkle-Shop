6d71c6fdf28e5e2dc8dce139ccc5418232583f3c
Tinatajik <tinatajikrostamii@gmail.com>
2023-11-30 14:17:05 +0330

    update-image

--- src/pages/mainPage/main/ShopLatest.js
+++ src/pages/mainPage/main/ShopLatest.js
@@ -2,39 +2,33 @@ export default function ShopLatest() {
   return (
     <>
       <div className="flex justify-center mt-5 gap-7">
-        <h3 className="bg-white w-[15%] text-4xl text-center py-14">
+        <h3 className="bg-white w-[15%] text-4xl text-center py-14 mt-2">
           Shop the latest from Instagram <br></br> Follow us
         </h3>
         <img
           className="w-[15%]"
-          src="https://scontent.cdninstagram.com/v/t51.29350-15/403631787_682873990484493_3959251284874918262_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=aNOUpy3yPEsAX9aXQqc&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&oh=00_AfBXTqgm-ivqMiLrvDSq939x2bdOvRrqwWdBhrZ81ZOumA&oe=65602278"
-        />
+          src="https://stonejewelry.gr/themes/at_athena/assets/img/modules/appagebuilder/images/earrings-600x900%20sept.jpg"/>
         <img
           className="w-[15%]"
-          src="https://scontent.cdninstagram.com/v/t51.29350-15/403058064_348584037854607_1164358187286619668_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=I0P6RETgnVMAX8_KbXx&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&oh=00_AfCdLqYENEQooQ1oXX9WxVLfbN_NJ0WXJDyWI1EOS1MMpA&oe=656152D0"
-        />
+          src="https://stonejewelry.gr/themes/at_athena/assets/img/modules/appagebuilder/images/bracelets-600x900%20sept.jpg"/>
         <img
           className="w-[15%]"
-          src="https://scontent.cdninstagram.com/v/t51.29350-15/402320743_1525059468347479_4768494096938833075_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=XEPD08_COg0AX9oQFHT&_nc_oc=AQkFLyoPqJupB3uzHq8qKnNIt0xpOMCLqrHx39QmCDbwle98KNjlwXMZgpIZvHF--dU&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&oh=00_AfB7IFXbc9LpnKZRPEgVE3POWqwaj6j6E7_UMUaWaF6sdw&oe=65605087"
-        />
-        <img
+          src="https://www.stoneandstrand.com/cdn/shop/files/homepage-shopping-icons-rings_87cbe88d-3233-4292-b701-432e64833e8b.jpg?v=1697014618" /><img
           className="w-[15%]"
-          src="https://scontent.cdninstagram.com/v/t51.29350-15/401911315_1182514259802563_9053657625435448164_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=y8rQIPtG-XwAX_CliyR&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&oh=00_AfBxJvzea2zUS8H4Sf70G5CcyDSKl3AevF-g5lxfBTAHcg&oe=655F87D9"
-        />
+          src="https://www.stoneandstrand.com/cdn/shop/files/homepage-shopping-icons-earrings_5433f51d-0726-4306-b970-731ccf05d4d2.jpg?v=1697014773"/>
       </div>
       <div className="flex justify-center mt-5 gap-7">
         <img
           className="w-[15%]"
-          src="https://scontent.cdninstagram.com/v/t51.29350-15/400258339_926449078812325_2277932697925937758_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=3rmB6_JWR9gAX8onz9O&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&oh=00_AfAr14pO71LLPDfglbWak3TXfFoEPneZAglDECE3yEGM2A&oe=655FAF57"
-        />
+          src="https://www.stoneandstrand.com/cdn/shop/files/homepage-shopping-icons-necklaces_4f84d332-8585-4dd3-bf7b-a9885f32c2af.jpg?v=1697014891"/>
         <img
           className="w-[15%]"
-          src="https://scontent.cdninstagram.com/v/t51.29350-15/399899525_1036808640898121_5374420273313540473_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=KaNU6hUBmlwAX-ZKSep&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&oh=00_AfDtGNawVzP7NpYl8-9ERNVSJJzvPQgl7wxIdJtKLtAB4Q&oe=65600850"
-        />
+          src="https://www.stoneandstrand.com/cdn/shop/files/homepage-shopping-icons-bracelets_70b6042c-7979-44fd-9a6c-e462e8638b1e.jpg?v=1697015101"/>
         <img
           className="w-[15%]"
-          src="https://scontent.cdninstagram.com/v/t51.29350-15/399559051_919770226344759_4577694863386491874_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=JmfCsMkPDgkAX9h7RXK&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&oh=00_AfBhCcwXDjyID1BwwYCrk1f-TmvCh3a7BgOOgaHok2txyA&oe=6561048B"
-        />
+          src="https://cdn.shopify.com/s/files/1/2197/1801/files/BOLD-RUBY-HEART-PINKY-SIGNET-RING-ON-BODY_350x.jpg?v=1699950838" />
+          <img className="w-[15%]"
+          src="https://cdn.shopify.com/s/files/1/2197/1801/files/LAB-CREATED-DIAMOND-BUBBLE-UP-SWELL-RING-ON-BODY_350x.jpg?v=1696602957" />
       </div>
     </>
   );
