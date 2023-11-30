40a5d20a45dabae2b6776f0c35a4e2f4f4023f92
Tinatajik <tinatajikrostamii@gmail.com>
2023-12-01 00:45:50 +0330

    handle the panel managment

--- src/pages/adminPage/productManagment/PanelManagment.js
+++ src/pages/adminPage/productManagment/PanelManagment.js
@@ -1,19 +1,38 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Nav from "./nav/Nav";
 import Products from "./products/Products";
 import StocksPrices from "./stocks-prices/StocksPrices";
 import Orders from "./order/Orders";
+
 export default function PanelManagment() {
+  const [selectedComponent, setSelectedComponent] = useState("Products");
+
+  const renderComponent = () => {
+    switch (selectedComponent) {
+      case "Products":
+        return <Products />;
+      case "StocksPrices":
+        return <StocksPrices />;
+      case "Orders":
+        return <Orders />;
+      default:
+        return null;
+    }
+  };
+
+  const handleNavClick = (componentName) => {
+    setSelectedComponent(componentName);
+  };
+
   return (
     <>
       <div className="flex justify-around mt-5">
-        <div>Panel Managment</div>
-        <Nav />
+        <div>Panel Management</div>
+        <Nav handleClick={handleNavClick} selectedComponent={selectedComponent} />
         <Link to="/adminPage">back AdminPage</Link>
       </div>
-      {/* <Products /> */}
-      {/* <StocksPrices /> */}
-      <Orders />
+      {renderComponent()}
     </>
   );
 }
